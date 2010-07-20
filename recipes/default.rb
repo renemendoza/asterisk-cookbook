bfe1f93ac7587061e9459c37de6f94fda84c173a
Rene Mendoza <latiendamac@gmail.com>
2010-07-20 14:50:25 -0500

    added cookbook data

---
+++ recipes/default.rb
@@ -0,0 +1,24 @@
+package "asterisk"
+#package "asterisk-mp3"
+
+service "asterisk" do
+  supports :restart => true, :reload => true, :status => :true, :debug => :true, "logger-reload" => true, "extensions-reload" => true,
+           "restart-convenient" => true, "force-reload" => true
+end
+
+template "/etc/asterisk/manager.conf" do
+  source "manager.conf.erb"
+  notifies :reload, resources(:service => "asterisk")
+end
+
+template "/etc/asterisk/sip.conf" do
+  source "sip.conf.erb"
+  notifies :reload, resources(:service => "asterisk")
+  external_ip = node[:ec2] ? node[:ec2][:public_ipv4] : node[:ipaddress]
+  variables (:external_ip => external_ip)
+end
+
+template "/etc/asterisk/extensions.conf" do
+  source "extensions.conf.erb"
+#  notifies "extensions-reload", resources(:service => "asterisk")
+end
